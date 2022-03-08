4960915fd3003f490ea6d77f945f02594562c56d
milax <milax@d415.lab.deim>
2022-03-08 13:52:48 +0100

    elelel

--- test.sh
+++ test.sh
@@ -1,37 +1,62 @@
 #! /bin/bash
 
+
+err() {
+	echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
+
 usage() {
 	echo "USAGE:"
-	echo "${0} [-h] [-f argsF] [-x argsX]"
+	echo "${0}"
+	echo "	[-h] Mostra help"
+	echo "	[-n nomFit] Nom del fitxer on guardar la config."
+	echo "	[-f numFiles] # Files de la zona (10..120)"
+	echo "	[-c numColum] # Columnes de la zona (10..36)"
+	echo "	[-p midaPort] Mida de la porteria (8..numFiles-1)"
+	echo "	[-0 posFilaPal,posColPal] Fila i columna de la paleta (2..118),(2..35)"
+	echo "	[-m midaPaleta] Mida de la paleta (3..numFiles-1)"
+	echo "	[-1 posFilaPil,posColPil,velFil,velCol] Fila, columna, velocitat vertical i horitzontal de la pilota en joc (2..118),(2..35),(-1.0..1.0),(-1.0..1.0). Aquesta opt es pot repetir fins a 9 vegades."
 }
 
 
 
 
-while getopts 'h:f:x:' arg; do
+while getopts ':hn:f:c:p:0:m:1:' arg; do
 	case "${arg}" in
 		h) 
-			echo "opcio h" 
+			echo "	[-h] Mostra help"
 			usage
 			exit 0
-			aflag='true'
-			dea="${OPTARG}"
-			echo "$OPTIND"
+			;;
+		n) 
+			echo "	[-n nomFit] Nom del fitxer on guardar la config."
 			;;
 		f) 
-			echo "opcio f"
-			bflag='true'
-			deb="${OPTARG}"
-			echo "$OPTIND"
+			echo "	[-f numFiles] # Files de la zona (10..120)"
 			;;
-		x) 
-			echo "opcio x"
-			file="${OPTARG}"
-			echo "$OPTIND"
+		c) 
+			echo "	[-c numColum] # Columnes de la zona (10..36)"
 			;;
-		*) 
-			echo "Unexpected option:\"${arg}\""
-			usage 1>&2
+		p) 
+			echo "	[-p midaPort] Mida de la porteria (8..numFiles-1)"
+			;;
+		0) 
+			echo "	[-0 posFilaPal,posColPal] Fila i columna de la paleta (2..118),(2..35)"
+			;;
+		m)
+			echo "	[-m midaPaleta] Mida de la paleta (3..numFiles-1)"
+			;;
+		1)
+			echo "	[-1 posFilaPil,posColPil,velFil,velCol] Fila, columna, velocitat vertical i horitzontal de la pilota en joc (2..118),(2..35),(-1.0..1.0),(-1.0..1.0). Aquesta opt es pot repetir fins a 9 vegades."
+			;;
+		:)
+			err "ERROR: Argument \"-${OPTARG}\" necesita parametres"
+			>&2 usage 
+			exit 1
+			;;
+		*)	
+			err "ERROR: Argument \"-${OPTARG}\" misteri"
+			>&2 usage 
 			exit 1
 			;;
 	esac
