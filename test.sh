29d3dae352bc1b678cea325fbd8cdbdb66dbce9c
tobias-vde <47950935+tobias-vde@users.noreply.github.com>
2022-03-15 13:56:51 +0100

    w0w

--- test.sh
+++ test.sh
@@ -20,6 +20,55 @@ usage() {
 
 
 
+FILENAME="" # Nom del fitxer on guardar la config
+
+Y=0 # Files
+X=0 # Cols
+WIDTH_GOAL=0 # Mida porteria
+
+Y_PADDLE=0 # Fila pal
+X_PADDLE=0 # Col pal
+WIDTH_PADDLE=0 # Mida pal
+
+N=0 # Num. de pilotas
+SERVES=""
+
+
+
+load_config() {
+	echo "loading config from \"$FILENAME\" :D"
+	while read -r line; do
+		echo $line
+	done < ${FILENAME}
+}
+
+save_config() {
+	echo "${Y} ${X} ${WIDTH_GOAL}" > ${FILENAME}
+	echo "${Y_PADDLE} ${X_PADDLE} ${WIDTH_PADDLE}" >> ${FILENAME}
+	for BALL in ${SERVES}; do
+		BALL_Y=$(echo $BALL | cut -d, -f1)
+		BALL_X=$(echo $BALL | cut -d, -f2)
+		BALL_SPEED_Y=$(echo $BALL | cut -d, -f3)
+		BALL_SPEED_X=$(echo $BALL | cut -d, -f4)
+		echo "firaball=(${BALL_Y}),(${BALL_X}), (${BALL_SPEED_Y}),(${BALL_SPEED_X})"
+	done
+}
+
+
+
+# test empty
+if [ -z "${Y}" ]; then
+	echo "hey"
+fi
+
+# greater than
+if [ "${Y}" -gt "0" ]; then
+	echo "y > 0"
+fi
+
+
+# exit 0
+
 
 while getopts ':hn:f:c:p:0:m:1:' arg; do
 	case "${arg}" in
@@ -30,6 +79,14 @@ while getopts ':hn:f:c:p:0:m:1:' arg; do
 			;;
 		n) 
 			echo "	[-n nomFit] Nom del fitxer on guardar la config."
+			if [ -z "${FILENAME}" ]; then
+				FILENAME=${OPTARG}
+				echo "FILENAME=${FILENAME}" # DEBUG
+				load_config
+			else
+				err "me has dado fixer 2 veces!"
+			fi
+
 			;;
 		f) 
 			echo "	[-f numFiles] # Files de la zona (10..120)"
@@ -47,7 +104,30 @@ while getopts ':hn:f:c:p:0:m:1:' arg; do
 			echo "	[-m midaPaleta] Mida de la paleta (3..numFiles-1)"
 			;;
 		1)
-			echo "	[-1 posFilaPil,posColPil,velFil,velCol] Fila, columna, velocitat vertical i horitzontal de la pilota en joc (2..118),(2..35),(-1.0..1.0),(-1.0..1.0). Aquesta opt es pot repetir fins a 9 vegades."
+			# greater than
+			for BALL in ${OPTARG}; do
+				if [ "${N}" -lt "9" ]; then
+					BALL_Y=$(echo $BALL | cut -d, -f1)
+					BALL_X=$(echo $BALL | cut -d, -f2)
+					BALL_SPEED_Y=$(echo $BALL | cut -d, -f3)
+					BALL_SPEED_X=$(echo $BALL | cut -d, -f4)
+					echo "reading ball #$N=(${BALL_Y}),(${BALL_X}), (${BALL_SPEED_Y}),(${BALL_SPEED_X})"
+					if (( $(echo "${BALL_SPEED_Y} >= -1.0" | bc -l) )) &&
+					 (( $(echo "${BALL_SPEED_Y} <=  1.0" | bc -l) )); then
+						echo ok1
+					else
+						echo no oka
+					fi
+
+					N=$((N+1))
+					SERVES="${SERVES} ${BALL}"
+				else
+					err "demasiadas bolas bro"
+					exit 0
+				fi
+			done
+			# echo "y > 0"
+			# echo "	[-1 posFilaPil,posColPil,velFil,velCol] Fila, columna, velocitat vertical i horitzontal de la pilota en joc (2..118),(2..35),(-1.0..1.0),(-1.0..1.0). Aquesta opt es pot repetir fins a 9 vegades."
 			;;
 		:)
 			err "ERROR: Argument \"-${OPTARG}\" necesita parametres"
@@ -61,3 +141,7 @@ while getopts ':hn:f:c:p:0:m:1:' arg; do
 			;;
 	esac
 done
+
+save_config
+
+
